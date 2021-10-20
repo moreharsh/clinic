f831ce0820c12ba235dc43fe2bb436edcf03a3a6
more-harsh <more-harsh@users.noreply.github.com>
2021-10-20 08:43:30 +0530

    Time Slots management php code added

--- appointment.php
+++ appointment.php
@@ -22,39 +22,74 @@ if(!empty($fullname) || !empty($email) ||  !empty($phone) ||  !empty($date) || !
     if(mysqli_connect_error()) {
       die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
     } else {
-      $SELECT = "SELECT booking_date FROM appointment WHERE booking_date = ? Limit 1";
-      $INSERT = "INSERT INTO appointment (fullname, email, phone, booking_date, booking_time) values (?, ?, ?, ?, ?)";
 
-      $stmt = $conn->prepare($SELECT);
-      $stmt->bind_param("ss",$date, $time);
-      $stmt->execute();
-      $stmt->bind_result($date, $time);
-      $stmt->store_result();
-      $rnum = $stmt->num_rows;
+      //To fetch data with given date, and time.
 
-      if($rnum == 0) {
-        $stmt->close();
+      // $query = "SELECT booking_date, booking_time FROM appointment WHERE booking_date = ? AND booking_time = ?";
+      //
+      // $count = 0;
+      //
+      // if ($stmt = $conn->prepare($query)) {
+      //
+      //   $stmt->bind_param("ss",$date, $time);
+      //     /* execute statement */
+      //     $stmt->execute();
+      //
+      //     /* bind result variables */
+      //     $stmt->bind_result($date, $time);
+      //
+      //     /* fetch values */
+      //     while ($stmt->fetch()) {
+      //         printf ("%s (%s)\n", $date, $time);
+      //         $count += 1;
+      //     }
+      //
+      //     /* close statement */
+      //     $stmt->close();
+      // }
+      //
+      // if($count)
+      // {
+      //     echo "Appointment Not Available <br>";
+      // }
+      // else
+      // {
+        $SELECT = "SELECT booking_date, booking_time FROM appointment WHERE booking_date = ? AND booking_time = ?";
+        $INSERT = "INSERT INTO appointment (fullname, email, phone, booking_date, booking_time) values (?, ?, ?, ?, ?)";
 
-        $stmt = $conn->prepare($INSERT);
-        $stmt->bind_param("sssss",$fullname,$email,$phone,$date,$time);
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("ss",$date, $time);
         $stmt->execute();
-        header("Location: appointment_confirm.html");
-      } else {
-        echo "Appointment Already Booked <br>";
-        // $query = "SELECT * FROM appointment";
-        // $data = mysqli_query($conn, $query);
-        //
-        // $count0 = 0;
-        //
-        // while (($result = mysqli_fetch_assoc($data))) {
-        //     echo $result['fullname'] . "  " . $result['email'] . "   " . $result['phone'] . "   " . $result['booking_date'] . "   " . $result['booking_time'], "<br>";
-        //
-        // }
-      }
-      $stmt->close();
-      $conn->close();
+        $stmt->bind_result($date, $time);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
 
-    }
+        if($rnum == 0) {
+          $stmt->close();
+
+          $stmt = $conn->prepare($INSERT);
+          $stmt->bind_param("sssss",$fullname,$email,$phone,$date,$time);
+          $stmt->execute();
+          header("Location: appointment_confirm.html");
+        } else {
+          echo "Appointment Already Booked <br>";
+
+          // "To print Data on website::==>"
+
+          // $query = "SELECT * FROM appointment";
+          // $data = mysqli_query($conn, $query);
+          //
+          // $count0 = 0;
+          //
+          // while (($result = mysqli_fetch_assoc($data))) {
+          //     echo $result['fullname'] . "  " . $result['email'] . "   " . $result['phone'] . "   " . $result['booking_date'] . "   " . $result['booking_time'], "<br>";
+          //
+          // }
+        }
+        $stmt->close();
+        $conn->close();
+      // }
+  }
 
 
 } else {
