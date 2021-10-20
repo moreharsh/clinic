e2280f03c7b56d1378e58ee2e1b2eae124a17ebe
more-harsh <more-harsh@users.noreply.github.com>
2021-10-20 20:30:12 +0530

    home page linked to appointment page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 setTimeout(function () {
     $('.loader_bg').fadeToggle();
 }, 5000);
+
+$(".btn").click(function(){
+	window.location.href='appointment.html';
+});
+
+$("#tour").click(function(){
+	window.location.href='#';
+});
