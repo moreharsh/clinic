288778da23ddbfe87c51f6fb242477202e5cc683
more-harsh <more-harsh@users.noreply.github.com>
2021-10-25 12:54:58 +0530

    teams&services page updated: almost done

--- js/team&services.js
+++ js/team&services.js
@@ -1,17 +1,20 @@
 $(".carousel").owlCarousel({
 margin: 10,
 loop: true,
+nav: true,
+navText: [
+  "<i class='fa fa-caret-left'></i>",
+  "<i class='fa fa-caret-right'></i>"
+],
 autoplay: true,
-autoplayTimeout: 2000,
+autoplayTimeout: 5000,
 autoplayHoverPause: true,
 responsive: {
     0:{
-        items: 1,
-        nav: false
+        items: 1
     },
     1000:{
-        items: 2,
-        nav: false
+        items: 2
     }
-} 
+}
 });
