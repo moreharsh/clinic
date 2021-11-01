233dac501160baf23438b7fb29eb6d18e1e9005a
more-harsh <more-harsh@users.noreply.github.com>
2021-11-01 11:28:03 +0530

    index page: almost done, carousel added

--- js/index.js
+++ js/index.js
@@ -13,3 +13,25 @@ $(".next").click(function(){
 $("#reviewBtn").click(function(){
 	window.location.href='review.html';
 });
+
+
+$(".carousel").owlCarousel({
+margin: 10,
+loop: true,
+nav: true,
+navText: [
+  "<i class='fa fa-angle-left'></i>",
+  "<i class='fa fa-angle-right'></i>"
+],
+autoplay: true,
+autoplayTimeout: 5000,
+autoplayHoverPause: true,
+responsive: {
+    0:{
+        items: 1
+    },
+    1000:{
+        items: 1
+    }
+}
+});
